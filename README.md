8b7eef98ee3ca4a44b6263b8fa4ae715530ee6a2
Creatide <hello@creatide.com>
2015-10-09 23:56:10 +0300

    First Version
    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+Update Text Layer Name
+======================
+
+Sketch plugin to update Text layer names from their own text values. Simple as that.
+
+### Install
+Copy `Update Text Layer Name.sketchplugin` folder to Sketch plugins folder. 
+
+#### How to Use
+Just select any number of item(s) and run the plugin. It goes through all of selected items and renames Text items based their own actual text string value. You can choose any number of groups and/or layers and it'll pick only Text items.
+
+1. Select layers and/or groups that contains Text elements.
+2. Use shortcut `ctrl` + `alt` + `cmd` + `T` or use `Plugins` menu.
+3. Now you text layer names should match with the actual text values.
+
+#### Shortcuts
+* Update Text Layer Name `ctrl` + `alt` + `cmd` + `T`
+
+### About
+I started to get bored to update "Text item layer names" manually. I just want to get their name to match actual text string.
---
+++ Update Text Layer Name.sketchplugin/Contents/Sketch/updateTextLayerName.js
@@ -0,0 +1,38 @@
+function updateTextLayerName(context) {
+
+    var doc = context.document;
+    var selection = context.selection;
+
+    if (selection.count() == 0) {
+        doc.showMessage("Select at least one item. Only Text layer names is affected.");
+        return;
+
+    } else {
+        loopLayers(selection, function (layer) {});
+    }
+
+    // Loop all layers even they are in groups
+    function loopLayers(layers, callback) {
+
+        for (var i = 0; i < [layers count]; i++) {
+
+            var layer = [layers objectAtIndex: i];
+
+            // Group
+            if ([layer isMemberOfClass: [MSLayerGroup class]]) {
+                callback(layer);
+                loopLayers([layer layers], callback);
+            }
+            // Layer
+            else {
+                callback(layer);
+                if ([layer isMemberOfClass: [MSTextLayer class]]) {
+                    layer.setName(layer.stringValue());
+                }
+
+            }
+
+        }
+    }
+
+};
