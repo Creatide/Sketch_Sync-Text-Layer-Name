ffcd681ef8cce36bb5c56a28122e970edca8bfd7
christiannam <hey@christiannam.com>
2019-10-02 15:23:58 -0500

    Update .sketchplugin name

---
+++ Sync Text Layer Name.sketchplugin/Contents/Sketch/syncTextLayerName.js
@@ -0,0 +1,39 @@
+function syncTextLayerName(context) {
+
+    var doc = context.document;
+    var selection = context.selection;
+
+    if (selection.count() == 0) {
+        doc.showMessage("Select at least one text layer.");
+        return;
+
+    } else {
+        loopLayers(selection, function (layer) {});
+    }
+
+    // Loop all layers even they are in groups
+    function loopLayers(layers, callback) {
+
+        for (var i = 0; i < [layers count]; i++) {
+
+            var layer = [layers objectAtIndex: i];
+
+            // Group
+            if ([layer isMemberOfClass: [MSLayerGroup class]]) {
+                callback(layer);
+                loopLayers([layer layers], callback);
+            }
+            // Layer
+            else {
+                callback(layer);
+                if ([layer isMemberOfClass: [MSTextLayer class]]) {
+                    layer.setName(layer.stringValue());
+                    layer.setNameIsFixed(false);
+                }
+
+            }
+
+        }
+    }
+
+};
